fd060ec7e058358eb91187e79ebaa28d86554e9d
laboratorio <laboratorio@UPOAULA10308>
2024-10-01 07:45:42 -0500

    Agregar archivos de proyecto.

---
+++ PaulLarrea_Clase2MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaulLarrea_Clase2MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
